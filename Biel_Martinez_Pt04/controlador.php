4f68ef8e9c822e064cb5c5f80b79dcd60bea09ae
bielmartinez <bielmartinezcaceres16@gmail.com>
2023-10-18 16:46:38 +0200

    comprovacions dades

--- Biel_Martinez_Pt04/controlador.php
+++ Biel_Martinez_Pt04/controlador.php
@@ -4,4 +4,12 @@
         $connexio = new PDO('mysql:host=localhost;dbname=pt04_Biel_Martinez', 'root', '');  
         return $connexio;
     }
+    
+    function arreglarDades($input)
+    {
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+        return $input;
+    }
 ?>
--- Biel_Martinez_Pt04/registrar.php
+++ Biel_Martinez_Pt04/registrar.php
@@ -2,3 +2,32 @@
 <?php
 include 'controlador.php';
 $connexio = conectarBD();
+if (isset($_REQUEST['usuari'])) {
+    $usuari =  arreglarDades($_POST['usuari']);
+    $contrasenya = arreglarDades($_POST['contrasenya']);
+    $contrasenya2 = arreglarDades($_POST['contrasenya2']);
+    $contrasenya = hash('sha512', $contrasenya);
+    if (empty($usuari)) {
+        $errors[0] = "El camp usuari està buit";
+    } else {
+        $correcte[0] = true;
+    }
+
+    if (empty($contrasenya)) {
+        $errors[1] = "El camp contrasenya està buit";
+    } else {
+        $correcte[1] = true;
+    }
+
+    if (empty($contrasenya2)) {
+        $errors[2] = "La segona contrasenya està buida";
+    } else {
+        $correcte[2] = true;
+    }
+
+    if ($contrasenya == $contrasenya2) {
+        $correcte[3] = true;
+    } else {
+        $errors [3] = "Les contrasenyes no coincideixen";
+    }
+}
